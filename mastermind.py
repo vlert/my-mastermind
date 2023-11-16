9b01b8f9edc3b849ee9ac0c2ddcb7be9a12e11c6
Supakorn Wanuchsoontorn <supakorn.wan@ku.th>
2023-11-16 07:31:14 +0700

    add setup

--- mastermind.py
+++ mastermind.py
@@ -4,3 +4,10 @@ class Mastermind:
         self.positions = positions
         self.solution = None
         self.guesses = []
+
+    def setup_game(self):
+        """
+        Sets up the game with a random solution.
+        """
+        import random
+        self.solution = [random.randint(1, self.colors) for _ in range(self.positions)]
